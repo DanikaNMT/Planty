175ce538dba957047a4f5f5b628e573a219a6c33
Arno Deceuninck <arnodece@gmail.com>
2025-09-06 22:10:55 +0200

    Add basic .NET structure

---
+++ backend/tests/Planty.API.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Planty.API.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ backend/tests/Planty.Application.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Planty.Application.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
