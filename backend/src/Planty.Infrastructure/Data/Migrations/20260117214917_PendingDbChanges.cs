42a7104e5b78652f331d141c14b21b517ab2f156
Arno Deceuninck <arnodece@gmail.com>
2026-01-17 22:49:41 +0100

    Add empty migration

---
+++ backend/src/Planty.Infrastructure/Data/Migrations/20260117214917_PendingDbChanges.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Planty.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PendingDbChanges : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
